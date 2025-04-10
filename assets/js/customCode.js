58553481c16d4df83e277e440a16e5f9347f3dde
belunatic <abelluvyou@yahoo.com>
2025-04-10 13:36:30 -0700

    a script to show and hide my work section

---
+++ assets/js/customCode.js
@@ -0,0 +1,21 @@
+let site = document.querySelector("#site");
+let fullStack = document.querySelector("#full-stack");
+
+let siteButton = document.querySelector("#showSite");
+let fullStackButton = document.querySelector("#showFullStack");
+
+siteButton.addEventListener("click", (e) => {
+	e.preventDefault();
+	site.classList.remove("hide");
+	fullStack.classList.add("hide");
+	siteButton.classList.add("primary");
+	fullStackButton.classList.remove("primary");
+});
+
+fullStackButton.addEventListener("click", (e) => {
+	e.preventDefault();
+	fullStack.classList.remove("hide");
+	site.classList.add("hide");
+	siteButton.classList.remove("primary");
+	fullStackButton.classList.add("primary");
+});
